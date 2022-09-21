2762b09467562c11ce72d740476fe2b65f2ec1f5
chinmay2312acc <113020864+chinmay2312acc@users.noreply.github.com>
2022-09-22 00:45:59 +0100

    Add files via upload

---
+++ formulas.js
@@ -0,0 +1,85 @@
+// circle 
+
+let r = 10; 
+const pi = 3.14 ;
+Area_circle = pi*(r*r) ;
+console.log("the area of circle is = " + Area_circle) ; 
+
+circumerence_circle = (2 * pi*r) 
+console.log("the circumference of circle = " + circumerence_circle) 
+
+// circle 
+
+// rectangle 
+
+let length = 14 ; 
+let width = 10 ; 
+let height  = 4 ;
+const area_rectangle = length * width ; 
+console.log("the area of rectangle = " +area_rectangle) 
+
+ // rectangle 
+
+ // triangle 
+
+ let b = 10 ;
+ let h = 3 ;
+ let half = 1/2 ;
+let area_triangle = half*h*b ;
+console.log("area of triangle = "+ area_triangle) ;
+
+ // triangle 
+
+ // pythogras 
+
+ let b1 = 2 ;
+ let a1 = 3 ;
+ let c = 2 ;
+ let c_square = c*c;
+ if(c_square= ((a1*a1)+(b1*b1))){
+     console.log("the dimensions are valid " );
+ }
+ else {
+
+    console.log("the dimensions are not valid")
+ } 
+ //pythogras 
+
+ // cube
+ 
+ let l1 = 10 ;
+ let w2 = 10 ;
+ let h3 = 10 ;
+
+ const volume = l1*w2*h3 ;
+
+ console.log("the volume of cube = " + volume ) ;
+ 
+ //cube 
+
+// cylinder 
+
+let l2 = 10 ;
+let h4 = 10 ;
+
+let V_cylinder = pi*l2*h4 ;
+console.log("The volume of cylinder = " + V_cylinder) ; 
+
+//cylinder 
+
+// sphere
+
+let radi = 10 ;
+
+let v_sphere = (4/3)*pi*(radi*radi*radi)
+
+console.log("the volume of sphere = "+ v_sphere) ;
+
+// sphere
+
+
+
+ 
+
+
+
